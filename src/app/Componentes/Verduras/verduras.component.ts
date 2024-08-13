615963dce14d49990e521d55e32bbeca4c0a3989
Juan Manuel Triana Gallego <juan.triana@o365.unab.edu.co>
2024-08-12 21:56:29 -0500

    primer commit

---
+++ src/app/Componentes/Verduras/verduras.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verduras',
+  standalone: true,
+  imports: [],
+  templateUrl: './verduras.component.html',
+  styleUrl: './verduras.component.scss'
+})
+export class VerdurasComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FrutasComponent } from './Componentes/Frutas/frutas.component';
+import { VerdurasComponent } from './Componentes/Verduras/verduras.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FrutasComponent,VerdurasComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
